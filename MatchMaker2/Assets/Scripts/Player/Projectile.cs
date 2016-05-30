69216c5407f544c440ebdb3feeb23de729b22afc
nathan-san <18964@ma-web.nl>
2016-05-30 17:24:15 +0200

    Made basis of bullet script and added simple explosion particles

---
+++ MatchMaker2/Assets/Scripts/Player/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+    private Rigidbody2D rb;
+    [SerializeField]
+    private GameObject explosionParticle;
+    [SerializeField]
+    private float xSpeed = 5;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        SetVelocity(xSpeed, 0);
+    }
+    public void SetVelocity(float _xSpeed, float _ySpeed)
+    {
+        rb.velocity = new Vector2(_xSpeed,_ySpeed);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        CreateExplosion();
+        Destroy(this.gameObject);
+    }
+    void CreateExplosion()
+    {
+        for (int i = 0; i < 10; i ++)
+        {
+            GameObject temp =  Instantiate(explosionParticle, transform.position, Quaternion.identity)as GameObject;
+            temp.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-10,10), Random.Range(-10, 10));
+        }
+    }
+}
