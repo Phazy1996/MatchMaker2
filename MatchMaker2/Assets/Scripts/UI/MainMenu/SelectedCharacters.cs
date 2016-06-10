88c7e4dbdde0e037ba57f6aa8a334cd690306d12
Phineas Moore <phineasmoore@gmail.com>
2016-06-10 12:34:23 +0200

    Phineas Lobby first bit

---
+++ MatchMaker2/Assets/Scripts/UI/MainMenu/SelectedCharacters.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectedCharacters : MonoBehaviour {
+
+    [SerializeField]
+    private int playerNumber;
+
+    private int characterCount = 1;
+
+    private bool activated = false;
+
+    [SerializeField]
+    private int characterAmount;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ChangeCharacterCount(int value)
+    {
+        characterCount += value;
+
+        if (characterCount > characterAmount)
+        {
+            characterCount = 1;
+        }
+        else if (characterCount < 1)
+        {
+            characterCount = characterAmount;
+        }
+
+        Debug.Log(characterCount + "For player:" + playerNumber);
+    }
+}
